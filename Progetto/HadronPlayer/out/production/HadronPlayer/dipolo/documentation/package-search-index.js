7911f6f2e517427640fcfa1019d8d109f54f918c
SimoneA27 <simoneamirato@gmail.com>
2022-11-29 12:01:35 +0100

    Cartella progetto

---
+++ Progetto/HadronPlayer/out/production/HadronPlayer/dipolo/documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dipolo"},{"l":"dipolo.board"},{"l":"dipolo.heuristic"},{"l":"dipolo.research"}];updateSearchResults();
---
+++ Progetto/documentazione client/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"hadron"},{"l":"hadron.board"},{"l":"hadron.heuristic"},{"l":"hadron.research"}];updateSearchResults();
