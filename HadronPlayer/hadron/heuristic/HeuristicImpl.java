a538d64555a2bb53cac6acb7240a97ea32ed4607
francescopirox <f.pironti1999@gmail.com>
2022-12-09 10:25:57 +0100

    bugfixed

--- HadronPlayer/hadron/heuristic/HeuristicImpl.java
+++ HadronPlayer/hadron/heuristic/HeuristicImpl.java
@@ -26,7 +26,7 @@ public class HeuristicImpl implements Heuristic {
                 } else {
                     if( cellaBloccata(b , i , j) )
                         cb += 1;
-                    if( cellaSafe(b , col,i , j) )
+                    if( cellaSafe(b , col ,i , j) )
                         cs += 1;
                     if( cellaStrictSafe(b , i , j) )
                         css += 1;
@@ -37,16 +37,19 @@ public class HeuristicImpl implements Heuristic {
         if( css + cs % 2 == 1 && cb > 40 ) {
             dispari = 4;
         }
-        return pesi[0] * (9 * 9 - co) + pesi[1] * cb + pesi[2] * dispari * css + pesi[3] * cs;
+        System.err.println("Co: "+co+" -  Cb"+cb);
+        return pesi[0] * co + pesi[1] * cb + pesi[2] * dispari * css + pesi[3] * cs;
     }
 
     private static boolean cellaStrictSafe( Board b , int i , int j ) {
         int ret = 0;
-        for (int k = i - 1; k < i + 2 && k > -1 && k < 9; k += 2) {
-            ret += b.getPawn(k , j);
+        for (int k = i - 1; k < i + 2 ; k += 2) {
+            if(k > -1 && k < 9)
+                ret += b.getPawn(k , j);
         }
-        for (int h = j - 1; h < j + 2 && j > -1 && j < 9; h += 2) {
-            ret += b.getPawn(i , h);
+        for (int h = j - 1; h < j + 2 ; h += 2) {
+            if(h > -1 && h < 9)
+                ret += b.getPawn(i , h);
         }
         return ret == 0;
     }
@@ -55,7 +58,8 @@ public class HeuristicImpl implements Heuristic {
         int ret = 0;
         for (int k = i - 1; k < i + 2 && k > -1 && k < 9; k += 2)
             for (int h = j - 1; h < j + 2 && j > -1 && j < 9; h += 2) {
-                if( b.getCol(k , h) != col )
+
+                if(k > -1 && k < 9 && h > -1 && h < 9 && b.getCol(k , h) != col )
                     return false;
             }
         return true;
@@ -65,11 +69,13 @@ public class HeuristicImpl implements Heuristic {
     private static boolean cellaBloccata( Board b , int i , int j ) {
 
         int ret = 0;
-        for (int k = i - 1; k < i + 2 && k > -1 && k < 9; k += 2) {
-            ret += b.getPawn(k , j);
+        for (int k = i - 1; k < i + 2 ; k += 2) {
+            if(k > -1 && k < 9)
+                ret += b.getPawn(k , j);
         }
-        for (int h = j - 1; h < j + 2 && j > -1 && j < 9; h += 2) {
-            ret += b.getPawn(i , h);
+        for (int h = j - 1; h < j + 2; h += 2) {
+            if(h > -1 && h < 9)
+                ret += b.getPawn(i , h);
         }
 
         return ret == 0;
