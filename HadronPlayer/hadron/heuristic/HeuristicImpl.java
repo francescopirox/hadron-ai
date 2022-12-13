2ccd055f841ecfbe8bb96cee2e9cd1ec8181a269
francescopirox <f.pironti1999@gmail.com>
2022-12-13 18:50:47 +0100

    faster

--- HadronPlayer/hadron/heuristic/HeuristicImpl.java
+++ HadronPlayer/hadron/heuristic/HeuristicImpl.java
@@ -13,8 +13,7 @@ public class HeuristicImpl implements Heuristic {
 
     @Override
     public double evaluate( Board b , int col ) {
-        if( b.isFinal() )
-            return -10000D;
+
         int co,cb,cs,css,csa;
         co=cb=cs=css=csa=0;
         int i,j;
@@ -26,18 +25,19 @@ public class HeuristicImpl implements Heuristic {
                     co += 1;
                 } else if (cellaBloccata(b, i, j))
                     cb += 1;
+
+                else if (cellaStrictSafe(b, i, j))
+                    css += 1;
                 else if (cellaSafe(b, 1 - col, i, j))
                     cs += 1;
                 else if (cellaSafe(b, col, i, j))
                     csa += 1;
-                else if (cellaStrictSafe(b, i, j))
-                    css += 1;
-                else if (cellaAngularStrictSafe(b, i, j))
-                    css += 1;
-                else if (cellaBorderSafe(b, i, j, 1 - col))
+                else if ((i==0  || i==8 || j==0 || j==8) && cellaBorderSafe(b, i, j, 1 - col))
                     cs += 1;
-                else if (cellaBorderSafe(b, i, j, col))
+                else if ((i==0  || i==8 || j==0 || j==8) && cellaBorderSafe(b, i, j, col))
                     csa += 1;
+                else if ((i==0 || i==8) &&( j==0 || j==8) && cellaAngularStrictSafe(b, i, j))
+                    css += 1;
             }
         int fattoreSS = 1;
         int fattoreS  = 1;
@@ -45,18 +45,21 @@ public class HeuristicImpl implements Heuristic {
         int fattoreCB = 1;
 
         if(co<5)
-            fattoreCB=5;
+            fattoreCB=2;
 
         if( css + cs % 2 == 0 && csa==0 && (9 * 9 - co-cb-css-cs)==0 ) {
             fattoreSS = 4;
             fattoreS = 2;
+            if( (csa+css+cs)==0 && b.isFinal() )
+                return -10000D;
         }
-        if( css+cs % 2 == 1 && cb+co > 75 ) {
+        if( css+cs % 2 == 1 && cb+co > 65 ) {
             if( css % 2 == 1 )
                 fattoreSS = -10;
             else
                fattoreS = -20;
         }
+
         return pesi[0] * (9 * 9 - co-cb-css-cs-csa) + pesi[1] * fattoreCB *(cb+co) + pesi[2] * fattoreSS * css+pesi[3]*csa*fattoreSA +pesi[4] * cs * fattoreS;
 
     }
@@ -78,8 +81,7 @@ public class HeuristicImpl implements Heuristic {
     }
 
     private boolean cellaAngularStrictSafe( Board b , int i , int j ) {
-        if(i!=0 || i!=8 || j!=0 || j!=8)
-            return false;
+
 
         // CASO VERTICE LARGO
         if(i==0 && j==0 && b.getCol(0,1)==-1 && b.getCol(1,0)==-1 && b.getCol(2,0)!=-1 && b.getCol(1,1)!=-1 && b.getCol(0,2)!=-1)
@@ -117,26 +119,28 @@ public class HeuristicImpl implements Heuristic {
     }
 
     private static boolean cellaSafe( Board b , int col , int i , int j ) {
-        for (int k = i - 1; k < i + 2 ; k += 2)
-            for (int h = j - 1; h < j + 2 ; h += 2) {
-
-                if(k > -1 && k < 9 && h > -1 && h < 9 && b.getCol(k , h) != col )
-                    return false;
-            }
+        if(i !=0 && j!=0 && b.getCol(i-1 , j-1)!= col)
+             return false;
+        else if(i !=8 && j!=0 && b.getCol(i+1 , j-1)!= col)
+            return false;
+        else if(i!=0 && j !=8  && b.getCol(i-1 , j+1)!= col)
+           return false;
+        else if(i!=8 && j !=8  && b.getCol(i+1 , j+1)!= col)
+            return false;
         return true;
     }
 
     private static boolean cellaBloccata( Board b , int i , int j ) {
 
         int ret = 0;
-        for (int k = i - 1; k < i + 2 ; k += 2) {
-            if(k > -1 && k < 9)
-                ret += b.getPawn(k , j);
-        }
-        for (int h = j - 1; h < j + 2; h += 2) {
-            if(h > -1 && h < 9)
-                ret += b.getPawn(i , h);
-        }
+       if(i !=0)
+            ret += b.getPawn(i-1 , j);
+        if(i !=8)
+            ret += b.getPawn(i+1 , j);
+        if(j !=0)
+            ret += b.getPawn(i , j-1);
+        if(j !=8)
+            ret += b.getPawn(i, j+1);
 
         return ret != 0;
     }
